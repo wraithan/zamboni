f93d93988700700937a1324792251619511e8a33
Chuck Harmston <charmston@mozilla.com>
2013-04-15 12:57:31 -0500

    Split off API docs (bug 854942)

---
+++ docs/api/conf.py
@@ -0,0 +1,12 @@
+import sys, os
+sys.path.append(os.path.abspath('../..'))
+from docs.conf import *
+
+
+master_doc = 'api/index'
+
+project = u'marketplace-api'
+version = release = '1.0'  # Should correspond to the API version number
+
+html_theme_path = ['../_themes']
+html_static_path = ['../_static']
